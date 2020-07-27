c15d37ae8d1b91c009e132e5cdb5258f48a0cb50
JohnMaroe <maroelijoao@gmail.com>
2020-07-27 18:39:42 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button'
+// import ButtonLink from './components/ButtonLink';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Johnflix Logo" />
+            </a>´
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo video
+            </Button>
+        </nav>
+    )
+}
+
+export default Menu;
