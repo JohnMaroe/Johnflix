e39db0c7b7f6543477b9790b97ff9a0c463bd9d5
JohnMaroe <maroelijoao@gmail.com>
2020-07-30 19:27:19 -0300

    Primeira parte das mudanças da 4° aula da imersão Mudanças no formulário, animação adicionada

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,57 +1,62 @@
+/* eslint-disable linebreak-style */
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
     descrição: '',
     cor: '',
-  }
+  };
 
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
-  
-  
+
   function setValue(key, value) {
     setValues({
       ...values,
       [key]: value,
-    })
+    });
   }
 
   function handleNomeCategoria(e) {
-    const name = e.target.getAttribute('name')
-    const { value } = e.target
-    setValue(name, value)
+    const name = e.target.getAttribute('name');
+    const { value } = e.target;
+    setValue(name, value);
   }
 
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
       <form onSubmit={function handleSubmit(e) {
         e.preventDefault();
 
         setCategorias([
           ...categorias,
-          values
+          values,
         ]);
 
         setValues(valoresIniciais);
-      }}>
+      }}
+      >
 
-        <FormField 
+        <FormField
           label="Nome da categoria"
           type="text"
           name="nome"
           value={values.nome}
           onChange={handleNomeCategoria}
-          input={true}
+          input
         />
 
-        <FormField 
+        <FormField
           label="Descrição"
           type="text"
           name="descrição"
@@ -60,36 +65,33 @@ function CadastroCategoria() {
           input={false}
         />
 
-        <FormField 
+        <FormField
           label="Cor"
           type="color"
           name="cor"
           value={values.cor}
           onChange={handleNomeCategoria}
-          input={true}
+          input
         />
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
       <ul>
-        {categorias.map((categoria, indice) => {
-          return (
-            <li key={`${categoria}${indice}`}>
-              {categoria.nome}
-            </li>
-          )
-        })}
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
       </ul>
-
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
